825df6aa45ea9baf47137879cb55870d2e5b1682
Kiall Mac Innes <kiall@managedit.ie>
2012-01-10 12:26:07 +0000

    Add skeleton for the ORM StateMachine extension

---
+++ classes/orm/statemachine.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * 
+ * @package    StateMachine
+ * @category   Libraries
+ * @author     Managed I.T.
+ * @copyright  (c) 2012 Managed I.T.
+ * @license    http://labs.managedit.ie/license
+ */
+ class ORM_StateMachine extends ORM {
+ 	
+ }
