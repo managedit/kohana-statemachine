369a316b7150e9b6d4565d2a21626ead34af811a
Kiall Mac Innes <kiall@managedit.ie>
2012-01-10 13:17:44 +0000

    Add method comments to ORM_StateMachine class

--- classes/orm/statemachine.php
+++ classes/orm/statemachine.php
@@ -29,26 +29,56 @@ class ORM_StateMachine extends ORM {
 		));
 	}
 
+	/**
+	 * Returns the transitions array
+	 * 
+	 * @return  array
+	 */
 	public function transitions()
 	{
 		return array();
 	}
 
+	/**
+	 * Returns the transition callbacks array
+	 * 
+	 * @return  array
+	 */
 	public function transition_callbacks()
 	{
 		return array();
 	}
 
+	/**
+	 * Returns the current state
+	 * 
+	 * @return  string
+	 */
 	public function state()
 	{
 		return $this->_statemachine->state();
 	}
 
+	/**
+	 * Checks if a transition between two states is acceptable.
+	 * 
+	 * @param   string  $state_to    Transition to state
+	 * @param   string  $state_from  Transition from state
+	 * @return  bool
+	 */
 	public function can_transition($state_to, $state_from = NULL)
 	{
 		return $this->_statemachine->can_transition($state_to, $state_from);
 	}
 
+	/**
+	 * Transitions to the supplied state, triggering any
+	 * necessary callbacks.
+	 * 
+	 * @param   string  $state_to    Transition to state
+	 * @throws  StateMachine_InvalidTransition_Exception
+	 * @return  void
+	 */
 	public function transition($state_to)
 	{
 		return $this->_statemachine->transition($state_to);
